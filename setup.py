dceca3c609635cbc52f6c5b74ac5d564ca454c92
Migalin Danila <me@miga.me.uk>
2017-03-08 11:11:40 +0000

    bump to 0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="cacus",
-    version="0.4",
+    version="0.5",
     author="Danila Migalin",
     author_email="me@miga.me.uk",
     url="https://github.com/beebeeep/cacus",
